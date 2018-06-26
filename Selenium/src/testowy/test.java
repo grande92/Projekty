ee7e72059faa33ac95910f75d57a162ab815a91f
Grande <Grande@DESKTOP-K5K4LOE>
2018-06-26 21:27:16 +0200

    Modular

--- Selenium/src/testowy/test.java
+++ Selenium/src/testowy/test.java
@@ -3,14 +3,34 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class test {
-	public static void main(String[] args) {
-     
-    	System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.wp.pl/");
-        driver.quit();
-       
-    }
 
+public class test {
+	static WebDriver driver;
+	static String browser;
+
+	public static void main(String[] args) {	
+		setBrowser();
+		setBrowserConfig();
+		runTest();
+	}
+	//setBrowser
+	public static void setBrowser(){
+		browser="Firefox";
+	}
+	//setBrowserConfig
+	public static void setBrowserConfig(){
+		if(browser.contains("Chrome")) {
+			System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");
+			driver = new ChromeDriver();
+		}
+		if(browser.contains("Firefox")) {
+			System.setProperty("webdriver.gecko.driver","C:\\geckodriver.exe");
+			driver = new FirefoxDriver();
+		}
+	}
+	//runTest
+	public static void runTest(){
+		driver.get("https://www.wp.pl/");
+		driver.quit();
+	}
 }
